ad2c936b0a59bea5ca5926008855f1ce176ec2b0
Charlie Steenhagen <csteen1005@gmail.com>
2026-01-01 21:07:29 -0600

    Added overall mist readme

---
+++ mist/README.mk
@@ -0,0 +1,46 @@
+# Mist
+
+**Mist** is my take on a full size keyboard, but with some upgrades I carried over from my second keyboard build--**Hermod**.
+
+## Why?
+1. First of all, I've addicted to designing custom keyboards now, so jot that down.
+2. I have two children now, and while I love my split keyboard, typing with one hand is 100 times more difficult and time consuming on a split.
+3. Related to above, I wanted a keyboard that would appeal to the non-split community, but also incorporated all the features I loved mose about my split build.
+
+## Features
+
+- **2.2" LCD Display**: A 2.2" LCD Display to show keyboard state, stats, and any fun animations I can think of.
+- **Thumb Clusters in Place of Spacebar**: I loved the thumb clusters from my split keyboard and the giant spacebar is such a waste of space!
+- **Per-Key RGB Lighting**: Vibrant, individually addressable RGB LEDs for endless customization.
+- **KS-33 + MX/Gateron Normal Profile Hot-Swappable Switch Sockets**: Support for either KS-33 low-profile or MX style (Cherry or Gateron Normal profile) switches.
+- **QMK Firmware**: Fully programmable and customizable using QMK.
+- **Standard QWERTY Layout and Keymap**: The layout we all grew up learning to type on!
+
+There are currently **3** different versions of of this keyboard. 
+
+- **Mk. I**: **I have not ordered nor tested this version** - This is the first version of this board with onboard ESD and USB data protection.
+- **Mk. I - UDB**: Identical to the first version, but with the ESD and USB protection offloaded to a unified daughter board. 
+- **Mk. II**: Also uses a UDB but with upgraded 1.5u footprints to support either Gateron Low profile stabilizers or standard MX PCB mounted stabilizers.
+ 
+## Specifications
+
+- **Microcontroller**: STM32G474RETx
+- **Display**: 2.2" LCD (320x240 resolution) per half.
+- **Lighting**: SK6812 Mini RGB LEDs.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](../LICENSE) file for details.
+
+## Acknowledgments
+
+- The QMK community for their amazing firmware.
+- Adafruit for the GFX library support.
+- Inspiration from the mechanical keyboard enthusiast community.
+
+---
+
+For more information, check out the [Modern Hobbyist YouTube channel](https://www.youtube.com/ModernHobbyist) for videos about this project!
+
+> Also, please let me know in an issue or on Discord if there are issues with this board or the supplementary files so I can get them updated!
+
